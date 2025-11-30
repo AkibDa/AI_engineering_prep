7a907980be4c584ee3579af8ad47e68d141531a0
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-11-30 10:59:04 +0530

    made a agent that can go to wikipedia and do math

---
+++ agents_intro.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_classic.agents import AgentType, initialize_agent, load_tools
+
+load_dotenv()
+
+llm=init_chat_model("google_genai:gemini-2.5-flash-lite")
+tools = load_tools(["wikipedia","llm-math"], llm=llm)
+agent = initialize_agent(tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+response = agent.run("What is the birth date of Albert Einstein? Also, what is 15% of that year?")
+print(response)
